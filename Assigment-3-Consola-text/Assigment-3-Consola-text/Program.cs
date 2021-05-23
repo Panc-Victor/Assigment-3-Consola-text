12e27ec4cd26922022a1bfb5d0bfb58e78d560c7
Panc-Victor <panc_vctr@yahoo.com>
2021-05-23 20:32:10 +0300

    Console Text

---
+++ Assigment-3-Consola-text/Assigment-3-Consola-text/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assigment_3_Consola_text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "This is my car!";
+
+            string[] result = message.Split(' ', (char)StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine($"Words count: {result.Length}");
+        }
+    }
+}
